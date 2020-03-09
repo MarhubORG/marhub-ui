ec1f9f20ca620614f78fb49913788de0f8bcb0cc
andygimma <andrewgimma@protonmail.com>
2020-03-09 14:54:15 -0500

    Add footer component, set up main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from './components/Nav/nav';
+import Footer from './components/Footer/Footer';
+
+const StyledLayout = styled.div`
+  display: flex;
+  min-height: 100vh;
+  max-height: 100vh;
+  flex-direction: column;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+  overflow-y: scroll;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <StyledLayout>
       <Nav />
-    </div>
+      <MainContent />
+      <Footer />
+    </StyledLayout>
   );
 }
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FaFacebookF, FaTwitter, FaLinkedin } from 'react-icons/fa';
+
+const StyledFooter = styled.footer`
+  display: flex;
+  background-color: white;
+  height: 1.3rem;
+  padding: 2rem 1.2rem;
+`;
+
+const MarhubLink = styled.a`
+  margin-left: 0.2rem;
+  color: #3366ff;
+  text-decoration: none;
+  font-weight: 600;
+`;
+
+const SocialLinksSection = styled.section`
+  margin-left: auto;
+`;
+
+const FacebookLink = styled(FaFacebookF)`
+  height: 1.55rem;
+  width: 1.55rem;
+  color: #8f9bb3;
+`;
+
+const TwitterLink = styled(FaTwitter)`
+  height: 1.55rem;
+  width: 1.55rem;
+  color: #8f9bb3;
+`;
+
+const LinkedInLink = styled(FaLinkedin)`
+  height: 1.55rem;
+  width: 1.55rem;
+  color: #8f9bb3;
+  padding-left: 0.2rem;
+`;
+
+const SocialLink = styled.a`
+  padding-left: 0.7rem;
+`;
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <span>Powered by </span>
+      <MarhubLink
+        href="https://www.marhubinternational.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Marhub
+      </MarhubLink>
+      <SocialLinksSection>
+        <SocialLink>
+          <FacebookLink />
+        </SocialLink>
+        <SocialLink>
+          <TwitterLink />
+        </SocialLink>
+        <SocialLink>
+          <LinkedInLink />
+        </SocialLink>
+      </SocialLinksSection>
+    </StyledFooter>
+  );
+}
