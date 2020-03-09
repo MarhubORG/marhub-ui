ac7b3e6c025c71cfba3c6aaefa1aa502025b4770
andygimma <andrewgimma@protonmail.com>
2020-03-09 11:17:11 -0500

    Add Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from './components/Nav/nav';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
