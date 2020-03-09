2015a00f5fcab63a305df0e677f1632ec7ef03c2
andygimma <andrewgimma@protonmail.com>
2020-03-09 16:40:48 -0500

    Set up stories to work with styled components ThemeProvider

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { mainTheme } from '../../styles/themes';
 import Footer from './Footer';
 
-storiesOf('Footer', module).add('Footer Component', () => <Footer />);
+storiesOf('Footer', module).add('Footer Component', () => (
+  <ThemeProvider theme={mainTheme}>
+    <Footer />
+  </ThemeProvider>
+));
--- src/components/Nav/Nav.stories.tsx
+++ src/components/Nav/Nav.stories.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { mainTheme } from '../../styles/themes';
 import Nav from './nav';
 
-storiesOf('Nav', module).add('Nav Component', () => <Nav />);
+storiesOf('Nav', module).add('Nav Component', () => (
+  <ThemeProvider theme={mainTheme}>
+    <Nav />
+  </ThemeProvider>
+));
