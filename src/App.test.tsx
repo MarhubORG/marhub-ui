c6e8d56dddc19383e931fc70706bf4a9cd61dd55
andygimma <andrewgimma@protonmail.com>
2020-03-08 19:00:50 -0500

    Fix Jest configuration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,8 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
+  const testJest = getByText(/test jest/i);
+
   expect(linkElement).toBeInTheDocument();
+  expect(testJest).toBeInTheDocument();
 });
