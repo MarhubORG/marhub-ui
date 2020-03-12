a826e645e2573a8cd87186e4b15dc69a95cea7ed
andygimma <andrewgimma@protonmail.com>
2020-03-12 10:20:18 -0500

    Login success. wip.

--- src/redux/sagas/registration.ts
+++ src/redux/sagas/registration.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import { put, takeLatest, all } from 'redux-saga/effects';
-import { SIGNUP } from '../constants/actionTypes';
-import { signupSuccess, signupError, SignupAction } from '../actions/index';
-
-const url = 'http://localhost:8080/api/v1/register';
+import { SIGNUP, LOGIN } from '../constants/actionTypes';
+import {
+  signupSuccess,
+  signupError,
+  SignupAction,
+  LoginAction,
+} from '../actions/index';
 
 function* signup(action: SignupAction) {
   try {
+    const url = 'http://localhost:8080/api/v1/register';
     yield axios.post(url, {
       name: action.payload.name,
       organisation: action.payload.organization,
@@ -23,6 +27,26 @@ function* actionWatcher() {
   yield takeLatest(SIGNUP, signup);
 }
 
+function* login(action: LoginAction) {
+  try {
+    console.log('here saga');
+    const url = 'http://localhost:8080/api/v1/login';
+    const json = yield axios.post(url, {
+      email: action.payload.email,
+      password: action.payload.password,
+    });
+    console.log({ json });
+    // yield put(signupSuccess());
+  } catch (error) {
+    console.log('error', error.response.data.message);
+    yield put(signupError(error.response.data.message));
+  }
+}
+
+function* watchLogin() {
+  yield takeLatest(LOGIN, login);
+}
+
 export default function* rootSaga() {
-  yield all([actionWatcher()]);
+  yield all([actionWatcher(), watchLogin()]);
 }
