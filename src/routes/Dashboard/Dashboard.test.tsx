5b57a06806a82d1e102106c092f0415d1c00c4ed
andygimma <andrewgimma@protonmail.com>
2020-03-31 20:01:22 -0500

    Update Dashboard tests

--- src/routes/Dashboard/Dashboard.test.tsx
+++ src/routes/Dashboard/Dashboard.test.tsx
@@ -1,17 +1,34 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import { UnconnectedDashboard, mapStateToProps } from './Dashboard';
 import { initialState } from '../../redux/reducers/registration';
 
-// it('renders correctly when logged in', () => {
-//   const tree = renderer.create(<UnconnectedDashboard isLoggedIn />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+const mockStore = configureMockStore();
+const store = mockStore({ registration: initialState });
 
-// it('renders correctly when not logged in', () => {
-//   const tree = renderer.create(<UnconnectedDashboard isLoggedIn />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+it('renders correctly when logged in', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <UnconnectedDashboard isLoggedIn />
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when not logged in', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <UnconnectedDashboard isLoggedIn />
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 describe('mapStateToProps', () => {
   it('contains registration state', () => {
