0e78b87355c1316d13bf28e6655e0e1da00f5ac7
andygimma <andrewgimma@protonmail.com>
2020-04-08 07:16:57 -0500

    Load template checkboxes

--- src/routes/Dashboard/OrganizationExportTemplate/OrganizationExportTemplate.tsx
+++ src/routes/Dashboard/OrganizationExportTemplate/OrganizationExportTemplate.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import styled from 'styled-components';
+import { databaseFields } from '../../../utils/database';
 
-interface OrganizationExportTemplate extends RouteComponentProps {
+interface OrganizationExportTemplateProps extends RouteComponentProps {
   organization?: string;
 }
-export default function OrganizationExportTemplate(
-  props: OrganizationExportTemplate
-): JSX.Element {
-  return <div>{props.organization} Template</div>;
+
+export default class OrganizationExportTemplate extends Component<
+OrganizationExportTemplateProps
+> {
+  createCheckboxes = () => {
+    return databaseFields.map(el => {
+      return (
+        <StyledCheckboxDiv key={el}>
+          <label htmlFor={el}>
+            <input type="checkbox" id={el} name={el} value="" />
+            {el}
+          </label>
+        </StyledCheckboxDiv>
+      );
+    });
+  };
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <StyledHeader>{this.props.organization} Template</StyledHeader>
+        <form>
+          <CheckboxesLayout>{this.createCheckboxes()}</CheckboxesLayout>
+          <br />
+        </form>
+      </div>
+    );
+  }
 }
+
+const CheckboxesLayout = styled.div`
+  max-height: 50vh;
+  padding-left: 2rem;
+`;
+
+const StyledHeader = styled.h1`
+  padding-left: 2rem;
+`;
+
+const StyledCheckboxDiv = styled.div`
+  padding: 0.1rem 0;
+`;
