adcf60e2ec0651f822aad54f365a8f1e04d603c5
andygimma <andrewgimma@protonmail.com>
2020-04-20 08:32:15 -0500

    Account is disabled button

--- src/components/Forms/Select/Select.tsx
+++ src/components/Forms/Select/Select.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-interface Option {
+export interface Option {
   value: string;
   name: string;
 }
--- src/routes/Dashboard/UserNew/UserNew.tsx
+++ src/routes/Dashboard/UserNew/UserNew.tsx
@@ -4,7 +4,15 @@ import styled from 'styled-components';
 import { RootState } from '../../../types/interfaces';
 import { Organization } from '../../../redux/actions/dashboard';
 import TextInput from '../../../components/Forms/TextInput/TextInput';
-import Select from '../../../components/Forms/Select/Select';
+import Select, { Option } from '../../../components/Forms/Select/Select';
+import {
+  MARHUB_ADMIN,
+  MARHUB_USER,
+  ADMIN,
+  USER,
+} from '../../../auth/permissionTypes';
+
+const roles = [MARHUB_ADMIN, MARHUB_USER, ADMIN, USER];
 
 interface UserEditProps {
   organizations: Organization[];
@@ -32,7 +40,7 @@ class UnconnectedUserNew extends Component<UserEditProps, UserEditState> {
     this.setState({ name });
   };
 
-  createOrganizationOptions = () => {
+  createOrganizationOptions = (): Option[] => {
     return this.props.organizations.map(el => {
       return {
         value: `${el.id}`,
@@ -41,8 +49,16 @@ class UnconnectedUserNew extends Component<UserEditProps, UserEditState> {
     });
   };
 
+  createRolesOptions = (): Option[] => {
+    return roles.map(el => {
+      return {
+        value: el,
+        name: el,
+      };
+    });
+  };
+
   render(): JSX.Element {
-    console.log(this.createOrganizationOptions());
     return (
       <Layout>
         <h1>New User</h1>
@@ -64,9 +80,18 @@ class UnconnectedUserNew extends Component<UserEditProps, UserEditState> {
         />
         <Select
           options={this.createOrganizationOptions()}
-          labelName="Organizations"
+          labelName="Select Organization:"
           selected=""
         />
+        <Select
+          options={this.createRolesOptions()}
+          labelName="Select Role:"
+          selected=""
+        />
+        <Label htmlFor="isDisabled">
+          Account is Disabled:
+          <input type="checkbox" id="isDisabled" name="isDisabled" />
+        </Label>
       </Layout>
     );
   }
@@ -84,5 +109,15 @@ export function mapStateToProps(state: RootState): MapStateToProps {
   const { organizations } = state.dashboardReducer;
   return { organizations };
 }
+
+const Label = styled.label`
+  display: block;
+  color: ${({ theme }): string => theme.grayText};
+  font-family: Open Sans, sans-serif;
+  font-size: 0.75rem;
+  font-weight: 700;
+  line-height: 1rem;
+  margin: 0.5rem 0rem;
+`;
 
 export default connect(mapStateToProps)(UnconnectedUserNew);
