0afa10055808cc7c0a82a918d10fa1e719a7cdb3
andygimma <andrewgimma@protonmail.com>
2020-05-18 02:56:32 -0500

    Change api ip

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const marhubUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://35.171.160.79:8080'
+    ? 'http://34.227.15.55:8080/'
     : 'http://localhost:8080';
 
 export const marhubApi = axios.create({
