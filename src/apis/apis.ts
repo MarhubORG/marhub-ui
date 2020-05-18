97086dbdce6c511b0cd2bc88a845a2f6cf654e5e
andygimma <andrewgimma@protonmail.com>
2020-05-18 00:39:34 -0500

    Change production port number

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const marhubUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://35.171.160.79:5000'
+    ? 'http://35.171.160.79:8080'
     : 'http://localhost:8080';
 
 export const marhubApi = axios.create({
