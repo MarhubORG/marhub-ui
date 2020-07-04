832820e4996081d82a21dfd0baaa3ae93dba4962
andygimma <andrewgimma@protonmail.com>
2020-07-04 11:19:51 -0500

    Only use localhost

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const marhubUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'http://34.227.15.55:8080/'
-    : 'http://localhost:8080';
+// export const marhubUrl =
+//   process.env.NODE_ENV === 'production'
+//     ? 'http://localhost:8080/'
+//     : 'http://localhost:8080';
+
+export const marhubUrl = 'http://localhost:8080';
 
 export const marhubApi = axios.create({
   baseURL: marhubUrl,
