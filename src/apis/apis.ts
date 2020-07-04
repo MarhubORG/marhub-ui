982f6012b63a20dfcc9377b18fbc4267fa79a663
andygimma <andrewgimma@protonmail.com>
2020-07-04 11:32:11 -0500

    Fix typo

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const marhubUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://http://www.monadashboard.org:8080'
+    ? 'http://www.monadashboard.org:8080'
     : 'http://localhost:8080';
 
 export const marhubApi = axios.create({
