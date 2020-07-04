1bf9417b5136993f1d55a2939670d814cefc946b
andygimma <andrewgimma@protonmail.com>
2020-07-04 12:29:33 -0500

    Debug statement

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -5,6 +5,7 @@ export const marhubUrl =
     ? 'http://www.monadashboard.org:8080'
     : 'http://localhost:8080';
 
+console.log('api url: ', marhubUrl);
 export const marhubApi = axios.create({
   baseURL: marhubUrl,
 });
