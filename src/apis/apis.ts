940087c9fd4945b056783974387d1b0d9683e523
andygimma <andrewgimma@protonmail.com>
2020-07-04 11:26:36 -0500

    Use domain as api url

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-// export const marhubUrl =
-//   process.env.NODE_ENV === 'production'
-//     ? 'http://localhost:8080/'
-//     : 'http://localhost:8080';
-
-export const marhubUrl = 'http://localhost:8080';
+export const marhubUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'http://http://www.monadashboard.org:8080'
+    : 'http://localhost:8080';
 
 export const marhubApi = axios.create({
   baseURL: marhubUrl,
