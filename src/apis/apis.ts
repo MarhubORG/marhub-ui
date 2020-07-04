4f0f5caec39fa46148febfc3ccd3f113af0486a5
andygimma <andrewgimma@protonmail.com>
2020-07-04 18:18:17 -0500

    Change api url

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const marhubUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://www.monadashboard.org:8080'
+    ? 'http://monadashboard.org:8080'
     : 'http://localhost:8080';
 
 console.log('api url: ', marhubUrl);
