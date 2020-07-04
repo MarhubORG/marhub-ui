bec4a58831932990c2a1e0e4e607960c2b9b97ca
andygimma <andrewgimma@protonmail.com>
2020-07-04 17:01:19 -0500

    Change api url

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-// export const marhubUrl =
-//   process.env.NODE_ENV === 'production'
-//     ? 'http://www.monadashboard.org:8080'
-//     : 'http://localhost:8080';
+export const marhubUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'http://www.monadashboard.org:8080'
+    : 'http://localhost:8080';
 
-export const marhubUrl = 'http://localhost:8080';
 console.log('api url: ', marhubUrl);
 export const marhubApi = axios.create({
   baseURL: marhubUrl,
