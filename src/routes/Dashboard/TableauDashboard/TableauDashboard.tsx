2587650f071d3beb230194152f0b53e8f678e70a
andygimma <andrewgimma@protonmail.com>
2020-07-13 11:07:59 -0500

    Update Tableau dashboard name

--- src/routes/Dashboard/TableauDashboard/TableauDashboard.tsx
+++ src/routes/Dashboard/TableauDashboard/TableauDashboard.tsx
@@ -42,7 +42,7 @@ export function DashboardObject(): JSX.Element {
           value="&#47;t&#47;marhubinternationaldashboard"
         />
         <param name="device" value="desktop" />
-        <param name="name" value="Dashboard_MONA_July" />
+        <param name="name" value="Dashboard_MONA_July&#47;Time" />
         <param name="tabs" value="yes" />
         <param name="toolbar" value="no" />
         <param name="showAppBanner" value="false" />
