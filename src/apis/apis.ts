cf2f761de51ca564c5db33a16b8e27ffc9bc5cf1
andygimma <andrewgimma@protonmail.com>
2020-09-05 14:32:51 -0500

    Remove heroku port

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const marhubUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://monabot-api-staging.herokuapp.com:8080'
+    ? 'https://monabot-api-staging.herokuapp.com'
     : 'http://localhost:8080';
 
 export const marhubApi = axios.create({
