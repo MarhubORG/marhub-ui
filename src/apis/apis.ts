9070166df1ec69e3344e142131d8854cfa838474
andygimma <andrewgimma@protonmail.com>
2020-09-05 14:20:56 -0500

    Use heroku port

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const marhubUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://monadashboard.org:8080'
+    ? 'https://monabot-api-staging.herokuapp.com:8080'
     : 'http://localhost:8080';
 
 export const marhubApi = axios.create({
