55422caf0ebf5224cb0796f78f326b74db656026
andygimma <andrewgimma@protonmail.com>
2020-09-07 20:52:03 -0500

    Redirect heroku https

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(function(req, res, next) {
   if (req.get('X-Forwarded-Proto') !== 'https') {
     res.redirect('https://' + req.get('Host') + req.url);
   } else next();
 });
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function(req, res) {
   return res.send('pong');
 });
